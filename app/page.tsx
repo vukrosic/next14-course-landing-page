f4019ffa8593cf2139506367fb9d8865d8203bdf
vukrosic <74423856+vukrosic@users.noreply.github.com>
2024-03-04 23:28:46 +0100

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex h-full w-full flex-col items-center justify-between p-24">
+      Hi
+    </main>
+  );
+}
---
+++ convex/auth.config.ts
@@ -0,0 +1,8 @@
+export default {
+    providers: [
+        {
+            domain: "https://concise-stork-19.clerk.accounts.dev",
+            applicationID: "convex",
+        },
+    ]
+};
